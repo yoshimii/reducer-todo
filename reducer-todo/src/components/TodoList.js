11bc288969237364ee3c4eb2c451ae50e7ea61d4
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-11 00:34:55 -0500

    last styling for the night

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
 
 const TodoList = (props) => {
     const { toggleDone, toggleKids } = props;
+    const classes = useStyles();
         return (
             <table>
                 <h2>Waiting List</h2>
@@ -13,13 +25,14 @@ const TodoList = (props) => {
                     <th>Seated</th>
                 </tr>
                 {props.state.map(item => {
+
                     return <>
                     <tr>
                     <td className={`todo-${item.completed ? 'completed' : ''}`}>{item.item}<span className={`present-${item.kids ? '' : 'true'}`}> 👶</span></td>
                     <td className={`todo-${item.completed ? 'completed' : ''}`}>{item.party}</td>
-                    <td><button onClick={() => toggleKids(item.id)}>Kids</button></td>
+                    <td><Button variant="outlined" color='primary' className={classes.margin} size='small' onClick={() => toggleKids(item.id)}>Kids</Button></td>
                     <td className={`todo-${item.completed ? 'completed' : ''}`}>{item.time}</td>
-                    <td><button onClick={() => toggleDone(item.id)}>Seated</button></td>
+                    <td><Button variant="outlined" color='primary' className={classes.margin} size='small' onClick={() => toggleDone(item.id)}>Seated</Button></td>
                     </tr>
                     </>
                 })}
